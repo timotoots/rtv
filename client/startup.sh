0371b99e22f337e78721114cee97463e6ad7f796
Timo Toots <timo@timo.ee>
2017-05-31 15:50:36 +0000

    startup

--- client/startup.sh
+++ client/startup.sh
@@ -4,12 +4,13 @@
 
 #while true; do
 
+     sleep 10
      /opt/rtv/client/video_server.py &
 #     node /opt/rtv/client/visual/draw.js &
      /opt/rtv/client/main.py &
 
 if [ "$HOSTNAME" = "rtv2" ]; then
-    python3 /opt/rtv/client/sweep_xz.py &
+    /opt/rtv/client/sweep_xz.py &
 fi
 
 
