97c554ab159a49587cc7921e210aee6d101860b1
Timo Toots <timo@timo.ee>
2017-06-01 09:13:28 +0000

    draw

--- client/visual/draw.js
+++ client/visual/draw.js
@@ -27,6 +27,49 @@ var root_group;
 
 var depth_i = 0;
 
+///////////////////////////////////////////////
+
+    // in pixels on screen
+    var  px_calib_x1 = 100;
+    var  px_calib_x2 = 1800;
+    var  px_calib_y1 = 100;
+    var  px_calib_y2 = 1000;
+
+    if(client_id=="rtv2"){
+
+        // in mm from mirror top
+        var mm_calib_x1 = 1447;
+        var mm_calib_x2 = 2563;
+        var mm_calib_y1 = 210;
+        var mm_calib_y2 = 803;
+
+    } else {
+
+        // in mm
+        var mm_calib_x1 = 1447;
+        var mm_calib_x2 = 2563;
+        var mm_calib_y1 = 210;
+        var mm_calib_y2 = 803;
+
+
+    }
+
+
+    var mm_calib_dist_x = mm_calib_x2 - mm_calib_x1;
+    var mm_calib_dist_y = mm_calib_y2 - mm_calib_y1;
+
+    var px_calib_dist_x = px_calib_x2 - px_calib_x1;
+    var px_calib_dist_y = px_calib_y2 - px_calib_y1;
+
+    var px_per_mm_x = px_calib_dist_x / mm_calib_dist_x;
+    var px_per_mm_y = px_calib_dist_y / mm_calib_dist_y;
+
+    var mm_screen_offset_from_mirror_y = mm_calib_y1 - px_calib_y1 / px_per_mm_y;
+    var mm_screen_offset_from_mirror_x = mm_calib_x1 - px_calib_x1 / px_per_mm_x;
+
+
+///////////////////////////////////////////////
+
 // MQTT CONNECT
 const mqtt = require('mqtt')  
 const client = mqtt.connect('mqtt://192.168.22.20')
@@ -126,6 +169,30 @@ client.on('message', (topic, message) => {
         all_poly[el_id].geometry(coords);
 
         console.log("MOVE poly ID " + el_id + coords);
+
+        ///////////////////////////////////////////////////////////////////
+
+    } else if(topics[1] === 'calibrate') {
+ 
+        el_id = 100;
+
+        if (typeof all_poly[el_id] === "undefined") {
+
+            // create element
+            console.log("NEW poly / ID:" + el_id);
+
+             all_poly[el_id] = gfx.createPolygon();
+             all_poly[el_id].fill("#FFFFFF");
+             root_group.add(all_poly[el_id]);
+             // all_poly_pos[el_id] = [-1000,-1000,-1000,-1000];
+
+        }
+
+
+        all_poly[el_id].geometry([px_calib_x1, px_calib_y1, px_calib_x2, px_calib_y1, px_calib_x2, px_calib_y2, px_calib_x1, px_calib_y2]);
+
+
+        console.log("MOVE calibrate ID " + el_id + coords);
 
 ///////////////////////////////////////////////////////////////////
 
@@ -241,30 +308,27 @@ client.on('message', (topic, message) => {
 
 function mm2px_x(value_mm){
 
+/*
     // substract screen offset in mm from point zero on glass
     if(client_id=="rtv1"){
-       var offset = 164;
+       var offset = 161;
     } else  if(client_id=="rtv2"){
        var offset = 1405;
     } else  if(client_id=="rtv3"){
        var offset = 2644;
     }
     value_mm = value_mm - offset;
+*/
 
-    return value_mm * 1.5259262635 + 38;
+    return (value_mm - mm_screen_offset_from_mirror_x)  *  px_per_mm_x;      
+
 
 
 }
 
 function mm2px_y(value_mm){
 
-    value_mm = value_mm ;
-
-
-
-    //  1843x1031px = 1210x677mm overscan rtv2
-
-    return value_mm *  1.5229262635  + 25;  
+    return (value_mm - mm_screen_offset_from_mirror_y)  *  px_per_mm_y;      
 
 }
 
