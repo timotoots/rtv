7532695d2a04b3a83bcbebff842cb23432d46d4a
Timo Toots <timo@timo.ee>
2017-06-02 15:03:50 +0000

    draw

--- client/visual/draw.js
+++ client/visual/draw.js
@@ -2,7 +2,7 @@
 
 var os = require('os');
 var client_id = os.hostname();
-var client_id = "rtv1";
+// var client_id = "rtv1";
 
 var path = require('path');
 var amino = require('./aminogfx-gl/main.js');
@@ -26,11 +26,25 @@ var all_face_pos = [];
 
 var all_poly = [];
 var all_depth = [];
+var games = {"square":0,"movement":0,"dots":0};
+
+
+if(client_id=="rtv1"){
+    games["square"] = 1;
+}
+if(client_id=="rtv2"){
+    games["movement"] = 1;
+}
+if(client_id=="rtv3"){
+    games["dots"] = 1;
+}
+
 
 var root_group;
 
 var depth_i = 0;
 
+var skip_frames_i= 0;
 ///////////////////////////////////////////////
 
 // Calibrarion data and functions
@@ -278,23 +292,29 @@ client.on('message', (topic, message) => {
              ////////////////////////
              // register new face
 
-            all_face[el_id] = {"landmarks":[], "last_landmarks_pos":map, "movement_timer":{"x_history":[],"y_history":[],"game_status":"standby","game_start_time":0}};
+            all_face[el_id] = {"landmarks":[], "hidden":1, "hide_timer":0, "last_landmarks_pos":map, "movement_timer":{"x_history":[],"y_history":[],"game_status":"standby","game_start_time":0}};
 
-            for (var i = 0; i < map.length; i++) {
+            /////////////////////////////////
+            // DOTS
 
-                var coords_x = mm2px_x(map[i][0]);
-                var coords_y = mm2px_y(map[i][1]);
-                all_face[el_id]["landmarks"][i] = gfx.createRect().x(coords_x-bar_width/2).y(coords_y).w(bar_width).h(bar_width).fill('#FFFFFF').opacity(1.0);
-                root_group.add(all_face[el_id]["landmarks"][i]);
+            if(games["dots"]==1){
 
 
-            } // for
+                for (var i = 0; i < map.length; i++) {
+
+                    var coords_x = mm2px_x(map[i][0]);
+                    var coords_y = mm2px_y(map[i][1]);
+                    all_face[el_id]["landmarks"][i] = gfx.createRect().x(coords_x-bar_width/2).y(coords_y).w(bar_width).h(bar_width).fill('#FFFFFF').opacity(1.0);
+                    root_group.add(all_face[el_id]["landmarks"][i]);
 
 
+                } // for
+
+             } // dots
 
             var coords_x = mm2px_x(map[3][0]);
             var coords_y = mm2px_y(map[3][1]);
-            all_face[el_id]["text"] =  gfx.createText().x(coords_x).y(coords_y).fontSize(40).fontName('ISOCTEUR').text('FACE '+el_id).fontWeight(200);
+            all_face[el_id]["text"] =  gfx.createText().x(coords_x).y(coords_y).fontSize(40).fontName('ISOCTEUR').text('').fontWeight(200);
             root_group.add(all_face[el_id]["text"]);
 
             console.log("Register new face:" + el_id);
@@ -303,110 +323,191 @@ client.on('message', (topic, message) => {
 
 
          ////////////////////////
-         // draw squares
+         // draw box
+ 
 
-        for (var i = 0; i < map.length; i++) {
+            if(games["square"]){
 
-            var coords_x = mm2px_x(map[i][0]);
-            var coords_y = mm2px_y(map[i][1]);
+            var coords_x_rect = mm2px_x(map[30][0])-100;
+            var coords_y_rect = mm2px_y((map[19][1]+map[24][1])/2-50);
 
-            var from_x = mm2px_x(all_face[el_id]["last_landmarks_pos"][i][0]);
-            var from_y = mm2px_y(all_face[el_id]["last_landmarks_pos"][i][1]);
+            var coords_w_rect = mm2px_x(map[17][0]-map[17][0]);
+            var coords_h_rect = mm2px_y(map[17][1]);          
+
+            if(typeof  all_rect[el_id+100] === "undefined"){
+
+                all_rect[el_id+100] = gfx.createRect().x(coords_x_rect).y(coords_y_rect).w(200).h(200).opacity(1.0);
+                root_group.add(all_rect[el_id+100]);
+                all_rect_pos[el_id+100] = [coords_x_rect,coords_y_rect];
+
+            } 
+                if(skip_frames_i>=0){
+                    all_rect[el_id+100].x.anim().from(all_rect_pos[el_id+100][0]).to(coords_x_rect).dur(200).start();
+                    all_rect[el_id+100].y.anim().from(all_rect_pos[el_id+100][1]).to(coords_y_rect).dur(200).start();
+                    skip_frames_i = 0;
+                }
+              
+            skip_frames_i++;
+      
+            
+            all_rect_pos[el_id+100] = [coords_x_rect,coords_y_rect];
+
+        }
+         
+         ////////////////////////
+         // draw dots
+
+         if(games["dots"]==1){
+
+            for (var i = 0; i < map.length; i++) {
+
+                var coords_x = mm2px_x(map[i][0]);
+                var coords_y = mm2px_y(map[i][1]);
+
+                var from_x = mm2px_x(all_face[el_id]["last_landmarks_pos"][i][0]);
+                var from_y = mm2px_y(all_face[el_id]["last_landmarks_pos"][i][1]);
 
 
-            // all_face[el_id]["landmarks"][i].x.anim().from( from_x).to(coords_x).dur(100).start();
-            // all_face[el_id]["landmarks"][i].y.anim().from( from_y).to(coords_y).dur(100).start();
+                // all_face[el_id]["landmarks"][i].x.anim().from( from_x).to(coords_x).dur(100).start();
+                // all_face[el_id]["landmarks"][i].y.anim().from( from_y).to(coords_y).dur(100).start();
 
-            all_face[el_id]["landmarks"][i].x(coords_x);
-            all_face[el_id]["landmarks"][i].y(coords_y);
-            // all_face[el_id]["landmarks"][i].opacity.anim().from(1).to(0).dur(3000).start();
-
-
-        } // for
+                all_face[el_id]["landmarks"][i].x(coords_x);
+                all_face[el_id]["landmarks"][i].y(coords_y);
+                // all_face[el_id]["landmarks"][i].opacity.anim().from(1).to(0).dur(3000).start();
 
 
+            } // for
 
-        var coords_x = mm2px_x(map[0][0]);
-        var coords_y = mm2px_y(map[0][1]);
+            if(all_face[el_id]["hidden"] == 1){
+                for (var i = 0; i < map.length; i++) {
+
+                    all_face[el_id]["landmarks"][i].opacity(1);
+                }
+            }
+
+
+
+        } // games dots
+
+            
+            if(all_face[el_id]["hidden"] == 1){
+    
+                 all_face[el_id]["text"].opacity(1);
+                 all_face[el_id]["hidden"] = 0;
+            }
+
+
+            clearTimeout(all_face[el_id]["hide_timer"]);
+
+            all_face[el_id]["hide_timer"] = setTimeout(function(el_id){
+
+                if(games["dots"]==1){
+
+                    for (var i = 0; i < map.length; i++) {
+                        all_face[el_id]["landmarks"][i].opacity(0);
+                     }
+
+                }
+
+                all_face[el_id]["text"].opacity(0);
+
+
+                all_face[el_id]["hidden"] = 1;
+              
+
+            },500,el_id);
+
      
 
         //////////////////////////////////////////////////////////////////
         /// Movement game
 
-	    var movement_samples = 4;
-        var movement_tolerance_mm = 100;
+        if(games["movement"]==1){
 
 
-        all_face[el_id]["movement_timer"]["x_history"].unshift(coords_x);
-        all_face[el_id]["movement_timer"]["y_history"].unshift(coords_y);
+            var coords_x = mm2px_x(map[0][0]);
+            var coords_y = mm2px_y(map[0][1]);
+
+    	    var movement_samples = 4;
+            var movement_tolerance_mm = 70;
 
 
-        if(all_face[el_id]["movement_timer"]["x_history"].length>movement_samples){
+            all_face[el_id]["movement_timer"]["x_history"].unshift(coords_x);
+            all_face[el_id]["movement_timer"]["y_history"].unshift(coords_y);
 
-            all_face[el_id]["movement_timer"]["x_history"].pop();
-            all_face[el_id]["movement_timer"]["y_history"].pop();
 
-        }
- 
+            if(all_face[el_id]["movement_timer"]["x_history"].length>movement_samples){
 
-        var total_x = 0;
-        var total_y = 0;
-        
-        for (var i = 0; i < all_face[el_id]["movement_timer"]["x_history"].length; i++) {
+                all_face[el_id]["movement_timer"]["x_history"].pop();
+                all_face[el_id]["movement_timer"]["y_history"].pop();
+
+            }
+     
+
+            var total_x = 0;
+            var total_y = 0;
             
-            total_x +=  all_face[el_id]["movement_timer"]["x_history"][i];
-            total_y +=  all_face[el_id]["movement_timer"]["y_history"][i];
-
-        }
-
-        var avg_x = total_x / all_face[el_id]["movement_timer"]["x_history"].length;
-        var avg_y = total_y / all_face[el_id]["movement_timer"]["y_history"].length;
-
-        if( Math.abs(avg_x - coords_x) > movement_tolerance_mm || Math.abs(avg_y - coords_y) > movement_tolerance_mm){
-
-            all_face[el_id]["movement_timer"]["x_history"] = [];
-            all_face[el_id]["movement_timer"]["y_history"] = [];
-
-            if(all_face[el_id]["movement_timer"]["game_status"]=="started"){
-
-                var d = new Date();
-                var time_result = d.getTime() - all_face[el_id]["movement_timer"]["game_start_time"];
-	      		time_result = Math.round(time_result/100);
-
-                all_face[el_id]["text"].text("YOU MOVED, LOSER! " + time_result);
-                all_face[el_id]["movement_timer"]["game_status"] = "show_result";
-                console.log("MOVE!");
-                all_face[el_id]["movement_timer"]["game_start_time"]  = 0;
-                setTimeout(function(el_id){
-
-               		all_face[el_id]["movement_timer"]["game_status"] = "standby";
-
-                },5000,el_id);
+            for (var i = 0; i < all_face[el_id]["movement_timer"]["x_history"].length; i++) {
+                
+                total_x +=  all_face[el_id]["movement_timer"]["x_history"][i];
+                total_y +=  all_face[el_id]["movement_timer"]["y_history"][i];
 
             }
 
-        } else if(all_face[el_id]["movement_timer"]["x_history"].length >= movement_samples && all_face[el_id]["movement_timer"]["game_status"]=="standby") {
+            var avg_x = total_x / all_face[el_id]["movement_timer"]["x_history"].length;
+            var avg_y = total_y / all_face[el_id]["movement_timer"]["y_history"].length;
 
-            all_face[el_id]["movement_timer"]["game_status"] = "started";
-            console.log("game started!");
-            all_face[el_id]["text"].text("DON'T MOVE!");
+            if( Math.abs(avg_x - coords_x) > movement_tolerance_mm || Math.abs(avg_y - coords_y) > movement_tolerance_mm){
 
-            var d = new Date();
-            all_face[el_id]["movement_timer"]["game_start_time"] =  d.getTime();
+                all_face[el_id]["movement_timer"]["x_history"] = [];
+                all_face[el_id]["movement_timer"]["y_history"] = [];
 
-			movement_timer(el_id);
+                if(all_face[el_id]["movement_timer"]["game_status"]=="started"){
+
+                    var d = new Date();
+                    var time_result = d.getTime() - all_face[el_id]["movement_timer"]["game_start_time"];
+    	      		time_result = Math.round(time_result/100);
+
+                    all_face[el_id]["text"].text("YOU MOVED! " + time_result);
+                    all_face[el_id]["text"].fill("#FF0000");
+
+                    all_face[el_id]["movement_timer"]["game_status"] = "show_result";
+                    console.log("MOVE!");
+                    all_face[el_id]["movement_timer"]["game_start_time"]  = 0;
+                    setTimeout(function(el_id){
+
+                   		all_face[el_id]["movement_timer"]["game_status"] = "standby";
+
+                    },5000,el_id);
+
+                }
+
+            } else if(all_face[el_id]["movement_timer"]["x_history"].length >= movement_samples && all_face[el_id]["movement_timer"]["game_status"]=="standby") {
+
+                all_face[el_id]["movement_timer"]["game_status"] = "started";
+                console.log("game started!");
+                all_face[el_id]["text"].text("DON'T MOVE!");
+                    all_face[el_id]["text"].fill("#FFFFFF");
+
+                var d = new Date();
+                all_face[el_id]["movement_timer"]["game_start_time"] =  d.getTime();
+
+    			movement_timer(el_id);
 
 
-        } 
+            } 
 
+        } // movement
 
         ////////////////////////////////////////////
 
 
+            var coords_x = mm2px_x(map[30][0])-200;
+            var coords_y = mm2px_y(map[30][1]);
 
         // move text
         all_face[el_id]["text"].x(coords_x);
-        all_face[el_id]["text"].y(coords_y+300);
+        all_face[el_id]["text"].y(coords_y+150);
 
 
         // save for next time
@@ -416,7 +517,7 @@ client.on('message', (topic, message) => {
 
 
 
-    } else if(topics[1] === 'face_text' && el_id) {
+    } else if(topics[1] === 'eyes' && el_id) {
 
 
         var msg = message.toString();
@@ -424,8 +525,8 @@ client.on('message', (topic, message) => {
 
          if(typeof all_face[el_id]["text"] != "undefined"){
 
-            all_face[el_id]["text"].text(msg);
-            console.log("NEW face text ID " + el_id + ": " +msg);
+            all_face[el_id]["text"].text("EYES " + msg);
+            console.log("NEW face eyes ID " + el_id + ": " +msg);
 
          }
 
