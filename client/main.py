93c2d4a5ea4386125d7637898f2054ddf6bae850
Timo Toots <timo@timo.ee>
2017-06-13 07:15:34 +0000

    main

--- client/main.py
+++ client/main.py
@@ -12,25 +12,25 @@ client_id = client_id + "_main"
 print(client_id)
 
 def on_message(client1, userdata, message):
-	print("message received  "  ,str(message.payload.decode("utf-8")))
-	msg = message.payload.decode("utf-8")
-	print(message.topic)
+    print("message received  "  ,str(message.payload.decode("utf-8")))
+    msg = message.payload.decode("utf-8")
+    print(message.topic)
 
-	if message.topic == "rtv_all/control":
+    if message.topic == "rtv_all/control":
 
-		if msg == "test":
+        if msg == "test":
 
-			output = subprocess.Popen(["ls", "-la"], stdout=subprocess.PIPE).communicate()[0]
-			output = output.decode("utf-8")
-			print (output)
-			client1.publish(client_id + "/control_log/"+msg,output)
+            output = subprocess.Popen(["ls", "-la"], stdout=subprocess.PIPE).communicate()[0]
+            output = output.decode("utf-8")
+            print (output)
+            client1.publish(client_id + "/control_log/"+msg,output)
 
-		if msg == "reboot":
+        if msg == "reboot":
 
-			output = subprocess.Popen(["reboot"], stdout=subprocess.PIPE).communicate()[0]
-			output = output.decode("utf-8")
-			print (output)
-			client1.publish(client_id + "/control_log/"+msg,output)
+            output = subprocess.Popen(["reboot"], stdout=subprocess.PIPE).communicate()[0]
+            output = output.decode("utf-8")
+            print (output)
+            client1.publish(client_id + "/control_log/"+msg,output)
 
                 if msg == "shutdown":
 
@@ -41,51 +41,51 @@ def on_message(client1, userdata, message):
 
 
 
-		if msg == "git_pull":
+        if msg == "git_pull":
 
 
-			output = subprocess.Popen(["git","pull"],  cwd=r'/opt/rtv/', stdout=subprocess.PIPE).communicate()[0]
-			output = output.decode("utf-8")
-			print (output)
-			client1.publish(client_id + "/control_log/"+msg,output)
+            output = subprocess.Popen(["git","pull"],  cwd=r'/opt/rtv/', stdout=subprocess.PIPE).communicate()[0]
+            output = output.decode("utf-8")
+            print (output)
+            client1.publish(client_id + "/control_log/"+msg,output)
 
-		if msg == "app_reboot":
+        if msg == "app_reboot":
 
-			output = subprocess.Popen(["killall","node"], stdout=subprocess.PIPE).communicate()[0]
-			output = output.decode("utf-8")
-			print (output)
-			client1.publish(client_id + "/control_log/"+msg,output)
+            output = subprocess.Popen(["killall","node"], stdout=subprocess.PIPE).communicate()[0]
+            output = output.decode("utf-8")
+            print (output)
+            client1.publish(client_id + "/control_log/"+msg,output)
 
-			subprocess.Popen(["node","/opt/rtv/client/visual/draw.js","&"])
-			client1.publish(client_id + "/control_log/"+msg,"draw started")
+            subprocess.Popen(["node","/opt/rtv/client/visual/draw.js","&"])
+            client1.publish(client_id + "/control_log/"+msg,"draw started")
 
-		if msg == "app_kill":
+        if msg == "app_kill":
 
-			output = subprocess.Popen(["killall","node"], stdout=subprocess.PIPE).communicate()[0]
-			output = output.decode("utf-8")
-			print (output)
-			client1.publish(client_id + "/control_log/"+msg,output)
-
-
-
-		# if msg == "sweep reboot":
+            output = subprocess.Popen(["killall","node"], stdout=subprocess.PIPE).communicate()[0]
+            output = output.decode("utf-8")
+            print (output)
+            client1.publish(client_id + "/control_log/"+msg,output)
 
 
 
-		# if msg == "camera reboot":    
+        # if msg == "sweep reboot":
+
+
+
+        # if msg == "camera reboot":    
 
 
 
 def on_publish(client,userdata,result):             #create function for callback
-	print("data published \n")
-	pass
+    print("data published \n")
+    pass
 
 
 
 def on_connect(client, userdata, flags, rc):
-	m="Connected flags"+str(flags)+"result code "\
-	+str(rc)+"client1_id  "+str(client)
-	print(m)
+    m="Connected flags"+str(flags)+"result code "\
+    +str(rc)+"client1_id  "+str(client)
+    print(m)
 
 client1 = paho.Client(client_id + "_control")                           #create client object
 client1.on_connect = on_connect        #attach function to callback
@@ -94,7 +94,7 @@ time.sleep(1)
 
 client1.connect(broker,port)      
 client1.subscribe("rtv_all/control")
-						   #establish connection
+                           #establish connection
 client1.loop_forever()    #start the loop
 # client1.publish("house/bulbs/bulb1","OFF")
 
