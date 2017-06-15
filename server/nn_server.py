bc950a25875fdae2d1e08d5de4dc6102716f6c64
Timo Toots <timo@timo.ee>
2017-06-15 22:16:37 +0300

    Tuned nearest neighbor server and added saving/loading the database.

--- server/nn_server.py
+++ server/nn_server.py
@@ -1,3 +1,5 @@
+import os.path
+import cPickle as pickle
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
@@ -5,28 +7,35 @@ from flask import Flask, Response, request
 app = Flask(__name__)
 
 #from werkzeug.contrib.profiler import ProfilerMiddleware
-#app.wsgi_app = ProfilerMiddleware(app.wsgi_app, profile_dir='nn_server_profile_json')
+#app.wsgi_app = ProfilerMiddleware(app.wsgi_app, profile_dir='nn_server_profile_sparse')
 
-nn = NearestNeighbors(n_neighbors=1)
-fit = False
-faces = []
-faces2persons = []
-persons = []
+nn = NearestNeighbors(n_neighbors=1, algorithm='auto')
+
+if os.path.isfile('nn_server.db'):
+    with open('nn_server.db', 'rb') as f:
+        faces, faces2persons, persons = pickle.load(f)
+    nn.fit(faces)
+else:
+    faces = []
+    faces2persons = []
+    persons = []
 
 @app.route('/', methods=['POST'])
 def search():
-    global fit
     # get JSON request
     content = request.get_json()
     face = np.array(content)
 
-    if fit:
+    # if database not empty
+    if faces:
         dists, ids = nn.kneighbors(face[np.newaxis])
         if dists[0, 0] < 0.6:
             person_id = faces2persons[ids[0, 0]]
-            faces2persons.append(person_id)
-            faces.append(face)
-            nn.fit(faces)
+            # only add new person if somewhat far from existing
+            if dists[0, 0] > 0.3:
+                faces2persons.append(person_id)
+                faces.append(face)
+                nn.fit(faces)
             return Response(persons[person_id], mimetype='application/json')
 
     person_id = len(persons)
@@ -34,8 +43,10 @@ def search():
     faces2persons.append(person_id)
     faces.append(face)
     nn.fit(faces)
-    fit = True
     return Response(persons[person_id], mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True)
+    with open('nn_server.db', 'wb') as f:
+        pickle.dump([faces, faces2persons, persons], f, protocol=pickle.HIGHEST_PROTOCOL)
+    print "faces: %d, persons: %d" % (len(faces), len(persons))
